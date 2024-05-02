ae9e68386c74561ee00543c736f9efc9d0cb3939
junkoyasu <160372603+junkoyasu@users.noreply.github.com>
2024-05-02 19:59:15 +0900

    php_005

---
+++ kadai_005/score.php
@@ -0,0 +1,25 @@
+<P>
+<?php
+//変数
+$score_1 = 80;
+$score_2 = 60;
+$score_3 = 55;
+$score_4 = 40;
+$score_5 = 100;
+$score_6 = 25;
+$score_7 = 80;
+$score_8 = 95;
+$score_9 = 30;
+$score_10 = 60;
+ 
+//合計の変数
+$score_total = $score_1 + $score_2 + $score_3 + $score_4 + $score_5 
++ $score_6 + $score_7 + $score_8 + $score_9 + $score_10;
+
+echo $score_total;
+
+echo '<br>';
+
+echo $score_total / 10;
+?>
+</p>
